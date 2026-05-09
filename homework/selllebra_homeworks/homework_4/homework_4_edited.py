ab11845cc70b697f2b3d2fda244f84803b1b869b
Tatsiana Mahilenka <tatsiana.mahilenka@gmail.com>
2026-05-09 23:02:08 +0200

    tuple key updated

--- homework/selllebra_homeworks/homework_4/homework_4_edited.py
+++ homework/selllebra_homeworks/homework_4/homework_4_edited.py
@@ -6,7 +6,7 @@ my_dict = {'tuple': (10, 20, 30, 40, 50),
 print(my_dict['tuple'][-1])
 my_dict['list'].append('coffee')
 my_dict['list'].pop(1)
-my_dict['dict']['message'] = 'i am a tuple'
+my_dict['dict'][('i am a tuple',)] = 'indeed'
 my_dict['dict'].pop('rain')
 my_dict['set'].add('Saturday')
 my_dict['set'].remove('Monday')
