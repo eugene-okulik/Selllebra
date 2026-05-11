8a612d0d33ee8fdc9d810c1b2e0d464004b60585
Tatsiana Mahilenka <tatsiana.mahilenka@gmail.com>
2026-05-11 11:47:12 +0200

    homework_5_lint_fix

--- homework/selllebra_homeworks/homework_5/homework_5.py
+++ homework/selllebra_homeworks/homework_5/homework_5.py
@@ -5,17 +5,17 @@ name, last_name, city, phone, country = person
 #Task 2
 line1 = 'результат операции: 42'
 index_start = line1.index(':')
-number = int(line1[index_start+2:])+10
+number = int(line1[index_start+2:]) + 10
 print(number)
 
 line2 = 'результат операции: 514'
 index_start = line2.index(':')
-number = int(line2[index_start+2:])+10
+number = int(line2[index_start+2:]) + 10
 print(number)
 
 line3 = 'результат работы программы: 9'
 index_start = line3.index(':')
-number = int(line3[index_start+2:])+10
+number = int(line3[index_start+2:]) + 10
 print(number)
 
 #Task 3
