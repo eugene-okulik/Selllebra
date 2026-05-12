fa5abc6e8586e0ffe35bfdb353209b0037770c25
Tatsiana Mahilenka <tatsiana.mahilenka@gmail.com>
2026-05-12 15:59:25 +0200

    homework_6_edited

--- homework/selllebra_homeworks/homework_6/homework_6_1.py
+++ homework/selllebra_homeworks/homework_6/homework_6_1.py
@@ -1,13 +1,3 @@
-"""
-Напишите программу,
- которая добавляет ‘ing’ в конец слов (к каждому слову)
- в тексте “Etiam tincidunt neque erat, quis molestie enim imperdiet vel.
- Integer urna nisl, facilisis vitae semper at, dignissim vitae libero”
- и после этого выводит получившийся текст на экран.
- Знаки препинания не должны оказаться внутри слова.
- Если после слова идет запятая или точка, этот знак препинания должен идти
- после того же слова, но уже преобразованного.
-"""
 line = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. \
 Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
 list_from_line = line.split(' ')
@@ -16,12 +6,11 @@ modified_list = []
 for word in list_from_line:
     if word.endswith(','):
         word = word.rstrip(',')
-        modified_list.append(word+'ing'+',')
+        modified_list.append(word + 'ing' + ',')
     elif word.endswith('.'):
         word = word.rstrip('.')
-        modified_list.append(word+'ing'+'.')
+        modified_list.append(word + 'ing' + '.')
     else:
-        modified_list.append(word+'ing')
+        modified_list.append(word + 'ing')
 result = ' '.join(modified_list)
 print(result)
-
