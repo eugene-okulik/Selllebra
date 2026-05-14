494116f85b58202a369fc8ae07309b477f735496
Tatsiana Mahilenka <tatsiana.mahilenka@gmail.com>
2026-05-14 20:55:06 +0200

    homework_7_edited

--- homework/selllebra_homeworks/homework_7/homework_7_1.py
+++ homework/selllebra_homeworks/homework_7/homework_7_1.py
@@ -3,4 +3,5 @@ user_number = int(input())
 while user_number != program_number:
     print('попробуйте снова')
     user_number = int(input())
+
 print('Поздравляю! Вы угадали!')
--- homework/selllebra_homeworks/homework_7/homework_7_2.py
+++ homework/selllebra_homeworks/homework_7/homework_7_2.py
@@ -3,4 +3,6 @@ words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
 def multiple_print(**kwargs):
     for word, count in kwargs.items():
         print(word * count)
+
+
 multiple_print(**words)
