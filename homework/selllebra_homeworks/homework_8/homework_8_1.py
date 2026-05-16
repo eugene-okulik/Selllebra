3dea755ba255abb8f62407a6d5cd15f8639cb87b
Tatsiana Mahilenka <tatsiana.mahilenka@gmail.com>
2026-05-16 18:40:57 +0200

    homework_8_lint_e712_fix

--- homework/selllebra_homeworks/homework_8/homework_8_1.py
+++ homework/selllebra_homeworks/homework_8/homework_8_1.py
@@ -4,6 +4,6 @@ import random
 salary = int(input('Write your salary'))
 bonus = random.choice([True, False])
 result = salary
-if bonus == True:
+if bonus is True:
     result = (salary + random.randrange(10, 10000, 100))
 print(f"{salary}, {bonus} - '${result}'")
