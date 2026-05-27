826ac604e7fc8ba8a8a9b05eba7da236bc49feb9
Tatsiana Mahilenka <tatsiana.mahilenka@gmail.com>
2026-05-27 15:00:47 +0200

    homework_11_comments_fix

--- homework/selllebra_homeworks/homework_11/homework_11_1.py
+++ homework/selllebra_homeworks/homework_11/homework_11_1.py
@@ -2,32 +2,34 @@ class Book:
     pages_material = "бумага"
     has_text = True
 
-    def __init__(self, title, author, pages, isbn, is_reserved):
+    def __init__(self, title, author, pages, isbn):
         self.title = title
         self.author = author
         self.pages = pages
         self.isbn = isbn
-        self.is_reserved = is_reserved
+        self.is_reserved = False
 
 
 book_1 = Book("Солярис", "Лем",
-              456, 1122334455667, "зарезервирована")
+              456, 1122334455667)
 book_2 = Book("Маленький принц", "Экзюпери",
-              678, 2233445566778, "не зарезервирована")
+              678, 2233445566778)
 book_3 = Book("1984", "Оруэлл",
-              567, 3344556677889, "не зарезервирована")
+              567, 3344556677889)
 book_4 = Book("Преступление и наказание", "Достоевский",
-              345, 4455667788990, "не зарезервирована")
+              345, 4455667788990)
 book_5 = Book("Идиот", "Достоевский",
-              500, 5566778899001, "не зарезервирована")
+              500, 5566778899001)
+
+book_1.is_reserved = True
 
 
 def book_print(book):
-    if book.is_reserved == "зарезервирована":
+    if book.is_reserved is True:
         print(
             f'Название: {book.title}, Автор: {book.author}, '
             f'страниц: {book.pages}, материал: {book.pages_material}, '
-            f'{book.is_reserved}'
+            f'зарезервирована'
         )
     else:
         print(
--- homework/selllebra_homeworks/homework_11/homework_11_2.py
+++ homework/selllebra_homeworks/homework_11/homework_11_2.py
@@ -1,56 +1,52 @@
-class Book:
-    pages_material = "бумага"
-    has_text = True
-
-    def __init__(self, title, author, pages, isbn, is_reserved):
-        self.title = title
-        self.author = author
-        self.pages = pages
-        self.isbn = isbn
-        self.is_reserved = is_reserved
+from Selllebra.homework.selllebra_homeworks.homework_11.homework_11_1 \
+    import (Book)
 
 
-class School_book(Book):
-    def __init__(self, title, author, pages, isbn, is_reserved,
-                 subject, level, has_tasks):
-        super().__init__(title, author, pages, isbn, is_reserved)
+class SchoolBook(Book):
+    def __init__(self, title, author, pages, isbn, subject, level):
+        super().__init__(title, author, pages, isbn)
         self.subject = subject
         self.level = level
-        self.has_tasks = has_tasks
+        self.has_tasks = True
 
 
-book_1 = School_book("Солярис", "Лем",
-                     456, 1122334455667, "зарезервирована",
-                     "литература", 11, True
-                     )
-book_2 = School_book("Римская империя", "Карлов",
-                     678, 2233445566778, "не зарезервирована",
-                     "история", 10, True
-                     )
-book_3 = School_book("Геометрия от А до Я", "Парлович",
-                     567, 3344556677889, "не зарезервирована",
-                     "математика", 9, True
-                     )
-book_4 = School_book("Народные песни", "Викорский",
-                     345, 4455667788990, "не зарезервирована",
-                     "пение", 11, True
-                     )
-book_5 = School_book("Букашки", "Букавко",
-                     100, 5566778899001, "не зарезервирована",
-                     "рисование", 1, True)
+book_1 = SchoolBook("Солярис", "Лем",
+                    456, 1122334455667,
+                    "литература", 11
+                    )
+book_2 = SchoolBook("Римская империя", "Карлов",
+                    678, 2233445566778,
+                    "история", 10
+                    )
+book_3 = SchoolBook("Геометрия от А до Я", "Парлович",
+                    567, 3344556677889,
+                    "математика", 9
+                    )
+book_4 = SchoolBook("Народные песни", "Викорский",
+                    345, 4455667788990,
+                    "пение", 11
+                    )
+book_5 = SchoolBook("Букашки", "Букавко",
+                    100, 5566778899001,
+                    "рисование", 1
+                    )
+
+
+book_5.is_reserved = True
 
 
 def school_book_print(book):
-    if book.is_reserved == "зарезервирована":
+    if book.is_reserved is True:
         print(
             f'Название: {book.title}, Автор: {book.author}, '
             f'страниц: {book.pages}, предмет: {book.subject}, '
-            f'{book.is_reserved}'
+            f'класс: {book.level}, зарезервирована'
         )
     else:
         print(
             f'Название: {book.title}, Автор: {book.author}, '
-            f'страниц: {book.pages}, предмет: {book.subject}'
+            f'страниц: {book.pages}, предмет: {book.subject}, '
+            f'класс: {book.level}'
         )
 
 
